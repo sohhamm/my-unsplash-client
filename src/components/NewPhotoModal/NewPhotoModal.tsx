35d859b7542e724d5415ff57f05f0e88682ddebb
Soham Sarkar <soham.ss1@gmail.com>
2021-12-22 21:54:58 +0530

    photo modal component

---
+++ src/components/NewPhotoModal/NewPhotoModal.tsx
@@ -0,0 +1,35 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from '@chakra-ui/react'
+
+declare interface PropTypes {
+  isOpen: boolean
+  onClose: () => void
+}
+
+export default function NewPhotoModal({isOpen, onClose}: PropTypes) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Modal Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>HI this is the body</ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+          <Button variant="ghost">Secondary Action</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,14 +1,21 @@
+import * as React from 'react'
 import {
   Flex,
   Input,
   InputGroup,
   InputLeftElement,
   Button,
+  useDisclosure,
 } from '@chakra-ui/react'
 import BrandLogo from '../../components/BrandLogo/BrandLogo'
 import {MdSearch} from 'react-icons/md'
 
+const NewPhotoModal = React.lazy(
+  () => import('../../components/NewPhotoModal/NewPhotoModal'),
+)
+
 export default function Header() {
+  const {isOpen, onOpen, onClose} = useDisclosure()
   return (
     <Flex p={'32px'}>
       <BrandLogo />
@@ -24,6 +31,8 @@ export default function Header() {
       <Button ml="auto" bgColor={'#3DB46D'} color="white">
         Add Photos
       </Button>
+
+      <NewPhotoModal isOpen={isOpen} onClose={onClose} />
     </Flex>
   )
 }
